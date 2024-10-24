9e9be48b12993dbfe3d1a78cb38c5d447967797a
NK.Shien_Home <shien091091@gmail.com>
2024-10-24 23:21:44 +0800

    場景初始化管理器  1.調整流程: 不管BindModel的順序, 只要有設定model順序就會依照設定依序執行CheckAddInitModelList

--- ProcessTools/SceneModelInitializer/SceneInitializeInstaller.cs
+++ ProcessTools/SceneModelInitializer/SceneInitializeInstaller.cs
@@ -1,14 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using SNShien.Common.TesterTools;
 using Zenject;
 
 namespace SNShien.Common.ProcessTools
 {
-    public abstract class SceneInitializeInstaller : MonoInstaller 
+    public abstract class SceneInitializeInstaller : MonoInstaller
     {
         private const string DEBUGGER_KEY = "SceneInitializeInstaller";
 
+        [InjectOptional] private IArchitectureModelSetting modelSetting;
+
         private ISceneModelInitializer sceneModelInitializer;
         private Debugger debugger;
+        private List<(Type modelType, Type modleInterfaceType)> waitForInitTypeList = new List<(Type modelType, Type modleInterfaceType)>();
 
         public override void InstallBindings()
         {
@@ -19,9 +25,26 @@ namespace SNShien.Common.ProcessTools
             ExecuteInstaller();
         }
 
-        private bool CheckAddInitModelList<T>()
+        private void InitModels()
         {
-            T resolve = Container.Resolve<T>();
+            if (modelSetting != null)
+                waitForInitTypeList = waitForInitTypeList
+                    .OrderBy(x => modelSetting.GetModelOrder(x.modelType.Name))
+                    .ToList();
+
+            foreach ((Type modelType, Type modelInterfaceType) in waitForInitTypeList)
+            {
+                debugger.ShowLog(CheckAddInitModelList(modelInterfaceType) ?
+                    $"init model success, model:{modelType.Name}" :
+                    $"init model failed, model:{modelType.Name}");
+            }
+            
+            waitForInitTypeList.Clear();
+        }
+
+        private bool CheckAddInitModelList(Type interfaceType)
+        {
+            object resolve = Container.Resolve(interfaceType);
             if (resolve is IArchitectureModel model)
             {
                 sceneModelInitializer.RegisterModel(model);
@@ -35,23 +58,20 @@ namespace SNShien.Common.ProcessTools
 
         private void Awake()
         {
+            InitModels();
             sceneModelInitializer.ExecuteAllModel();
         }
 
         protected void BindModel<T1, T2>() where T2 : T1
         {
             Container.Bind<T1>().To<T2>().AsSingle();
-            debugger.ShowLog(CheckAddInitModelList<T1>() ?
-                $"BindModel Success, model:{typeof(T2).Name}" :
-                $"BindModel Failed, model:{typeof(T2).Name}");
+            waitForInitTypeList.Add((typeof(T2), typeof(T1)));
         }
 
         protected void BindModelFromInstance<T1, T2>(T2 instance) where T2 : T1
         {
             Container.Bind<T1>().FromInstance(instance).AsSingle();
-            debugger.ShowLog(CheckAddInitModelList<T1>() ?
-                $"BindModelFromInstance Success, model:{typeof(T2).Name}" :
-                $"BindModelFromInstance Failed, model:{typeof(T2).Name}");
+            waitForInitTypeList.Add((typeof(T2), typeof(T1)));
         }
     }
 }
